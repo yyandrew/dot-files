a8ef33b68233b7cbd5d82df5b379ea1799e42f6f
yyandrew <qqerqqer@126.com>
2023-04-23 13:01:41 +0800

    添加重新加载lua配置文件的快捷键

--- .config/nvim/README.md
+++ .config/nvim/README.md
@@ -3,3 +3,4 @@
 
 ### 使用小技巧
 1. `Alt+q` - 将telescope里的 `live_grep` 结果显示在 quickfix 列表里；
+2. `luafile %` - 使当前lua配置生效；
--- .config/nvim/lua/keymappings.lua
+++ .config/nvim/lua/keymappings.lua
@@ -154,10 +154,10 @@ keymap.set("n", "<leader>fmg", "<cmd>CellularAutomaton game_of_life<CR>")
 
 -- telescope
 keymap.set('n', ',g', "<cmd>Telescope live_grep<CR>")
+keymap.set('n', ',G', "<cmd>Telescope grep_string<CR>")
 keymap.set('n', ',h', "<cmd>Telescope help_tags<CR>")
 keymap.set('n', ',d', "<cmd>Telescope diagnostics<CR>")
-keymap.set('n', '<Leader>a<space>', ':Ack! ')
-keymap.set('n', '<Leader>ac', ':Ack! <cword>')
+keymap.set('n', '<Leader>!', ":luafile %<CR>")
 
 require('nvim-tree').setup {
   update_focused_file = {
