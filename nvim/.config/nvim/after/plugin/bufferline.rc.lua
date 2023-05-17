cf900c57d6967fc7e2a501772973b8c3518a7cf1
yyandrew <qqerqqer@126.com>
2023-05-17 18:50:03 +0800

    修改bufferline配置

--- nvim/.config/nvim/after/plugin/bufferline.rc.lua
+++ nvim/.config/nvim/after/plugin/bufferline.rc.lua
@@ -5,16 +5,28 @@ end
 
 bufferline.setup {
   options = {
+    mode = "buffers",
     style_preset = {
       bufferline.style_preset.no_italic,
       bufferline.style_preset.no_bold
     },
+    indicator = {
+      style = "underline",
+    },
     numbers = function(opts)
       return string.format('%s·%s', opts.raise(opts.id), opts.lower(opts.ordinal))
     end,
+    truncate_names = false,
+    diagnostics = "nvim_lsp",
     diagnostics_indicator = function(count, level, diagnostics_dict, context)
       local icon = level:match("error") and " " or ""
       return " " .. icon .. count
     end,
+    highlights = {
+      tab_separator_selected = {
+        fg = 'white',
+        bg = 'red',
+      },
+    }
   }
 }
