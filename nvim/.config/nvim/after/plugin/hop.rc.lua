87d952cf4e9a629f154672e63fe5b2e98d9ed878
yyandrew <qqerqqer@126.com>
2023-06-05 17:20:24 +0800

    添加新的快速跳转的快捷键

---
+++ nvim/.config/nvim/after/plugin/hop.rc.lua
@@ -0,0 +1,18 @@
+local state, hop = pcall(require, "hop")
+if not state then
+  return
+end
+local directions = require('hop.hint').HintDirection
+local keymap = vim.keymap.set
+keymap('', 'f', function()
+  hop.hint_char1({ direction = directions.AFTER_CURSOR, current_line_only = true })
+end, {remap=true})
+keymap('', 'F', function()
+  hop.hint_char1({ direction = directions.BEFORE_CURSOR, current_line_only = true })
+end, {remap=true})
+keymap('', 't', function()
+  hop.hint_char1({ direction = directions.AFTER_CURSOR, current_line_only = true, hint_offset = -1 })
+end, {remap=true})
+keymap('', 'T', function()
+  hop.hint_char1({ direction = directions.BEFORE_CURSOR, current_line_only = true, hint_offset = 1 })
+end, {remap=true})
