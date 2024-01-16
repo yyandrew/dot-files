d9e6a85d210424bf45e14b608ac6717406588eb4
lq201 <qqerqqer@126.com>
2024-01-16 11:33:01 +0800

    Add lsp start and end keymaps

--- nvim/.config/nvim/lua/keymappings.lua
+++ nvim/.config/nvim/lua/keymappings.lua
@@ -216,3 +216,6 @@ keymap.set('i', '<C-g>', function () return vim.fn['codeium#Accept']() end, { ex
 keymap.set('i', '<c-;>', function() return vim.fn['codeium#CycleCompletions'](1) end, { expr = true })
 keymap.set('i', '<c-,>', function() return vim.fn['codeium#CycleCompletions'](-1) end, { expr = true })
 keymap.set('i', '<c-x>', function() return vim.fn['codeium#Clear']() end, { expr = true })
+
+keymap.set('', ',le', ':LspStop<CR>')
+keymap.set('', ',lb', ':LspStart<CR>')
