61e1c63e75e7452158e0a35e35c479ebd22a21f5
lq201 <qqerqqer@126.com>
2024-03-01 10:04:53 +0800

    Upgrade plugins of neovim

--- nvim/.config/nvim/after/plugin/telescope.rc.lua
+++ nvim/.config/nvim/after/plugin/telescope.rc.lua
@@ -1,7 +1,19 @@
+
+local state, telescope = pcall(require, 'telescope')
+if not state then
+  return
+end
+
+telescope.setup{
+  previewers = {
+
+  }
+}
+
+vim.cmd [[autocmd User TelescopePreviewerLoaded setlocal wrap]]
 local keymap = vim.keymap
 -- telescope
 keymap.set('n', ',g', "<cmd>Telescope live_grep<CR>")
 keymap.set('n', ',G', "<cmd>Telescope grep_string<CR>")
 keymap.set('n', ',h', "<cmd>Telescope help_tags<CR>")
 keymap.set('n', ',d', "<cmd>Telescope diagnostics<CR>")
-
