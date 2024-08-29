671e129618ae20a53b97298ee0dc40293755693d
lq201 <qqerqqer@126.com>
2024-08-29 18:35:56 +0800

    Replace default schedule_event and clean_schedule_event

--- nvim/.config/nvim/after/plugin/gitblame.rc.lua
+++ nvim/.config/nvim/after/plugin/gitblame.rc.lua
@@ -7,4 +7,6 @@ end
 gitblame.setup({
   date_format = "%Y-%m-%d %H:%M:%S", -- template for the date, check Date format section for more options
   virtual_text_column = 1,  -- virtual text start column, check Start virtual text at column section for more options
+  schedule_event = 'CursorHold',
+  clear_event = 'CursorHoldI',
 })
