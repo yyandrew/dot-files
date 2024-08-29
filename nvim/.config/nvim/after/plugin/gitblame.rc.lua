0e65a2393b4998483773af0233c64f568754c5f1
lq201 <qqerqqer@126.com>
2024-08-29 18:21:33 +0800

    modify gitblame message template

--- nvim/.config/nvim/after/plugin/gitblame.rc.lua
+++ nvim/.config/nvim/after/plugin/gitblame.rc.lua
@@ -5,7 +5,6 @@ if not state then
 end
 
 gitblame.setup({
-  message_template = " <summary> • <date> • <author> • <<sha>>",
   date_format = "%Y-%m-%d %H:%M:%S", -- template for the date, check Date format section for more options
   virtual_text_column = 1,  -- virtual text start column, check Start virtual text at column section for more options
 })
