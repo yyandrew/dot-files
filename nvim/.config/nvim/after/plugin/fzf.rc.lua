c78ec9a4f67a1f9cd0c515ef6fce9d5042304036
lq201 <qqerqqer@126.com>
2025-03-07 17:20:18 +0800

    user telescope buffers instead of fzf buffer

--- nvim/.config/nvim/after/plugin/fzf.rc.lua
+++ nvim/.config/nvim/after/plugin/fzf.rc.lua
@@ -2,8 +2,6 @@ local keymap = vim.keymap
 
 -- Ctrl + p open Git files
 keymap.set('n', ',p', ':GFiles --cached --others --exclude-standard<CR>')
--- ,e open buffers
-keymap.set('', ',e', '<cmd>:Buffers<CR>')
 keymap.set('', ',i', '<cmd>:Snippets<CR>')
 
 -- Config fzf
--- nvim/.config/nvim/after/plugin/telescope.rc.lua
+++ nvim/.config/nvim/after/plugin/telescope.rc.lua
@@ -14,6 +14,9 @@ telescope.setup{
       no_unlisted = true,
       line_width = 20,
     },
+    live_grep = {
+      line_width = 20,
+    }
   }
 }
 
@@ -21,6 +24,7 @@ vim.cmd [[autocmd User TelescopePreviewerLoaded setlocal wrap]]
 local keymap = vim.keymap
 -- telescope
 keymap.set('n', ',g', "<cmd>Telescope live_grep<CR>")
+keymap.set('n', ',e', "<cmd>Telescope buffers<CR>")
 keymap.set('n', ',G', "<cmd>Telescope grep_string<CR>")
 keymap.set('n', ',h', "<cmd>Telescope help_tags<CR>")
 keymap.set('n', ',d', "<cmd>Telescope diagnostics<CR>")
