a3086be9068045678b09a723d277cd0db3b581f8
lq201 <qqerqqer@126.com>
2025-03-08 21:02:16 +0800

    Add fzf-lua

--- nvim/.config/nvim/lua/plugins.lua
+++ nvim/.config/nvim/lua/plugins.lua
@@ -4,6 +4,14 @@ return {
   'kyazdani42/nvim-tree.lua',
   { 'junegunn/fzf',                  build = './install --bin', },
   'junegunn/fzf.vim',
+  {
+    "ibhagwan/fzf-lua",
+    -- optional for icon support
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    -- or if using mini.icons/mini.nvim
+    -- dependencies = { "echasnovski/mini.icons" },
+    opts = {}
+  },
   -- colorscheme
   'joshdick/onedark.vim',
   'morhetz/gruvbox',
