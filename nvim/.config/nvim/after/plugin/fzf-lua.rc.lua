e2ca5cbc53bab168d892b3ae0b8699f068f56bf1
lq201 <qqerqqer@126.com>
2025-03-10 09:10:20 +0800

    删除fzf

--- nvim/.config/nvim/after/plugin/fzf-lua.rc.lua
+++ nvim/.config/nvim/after/plugin/fzf-lua.rc.lua
@@ -9,9 +9,12 @@ fzf_lua.setup{
 
 local keymap = vim.keymap
 -- telescope
+keymap.set('n', ',p', '<cmd>FzfLua git_files<CR>')
+keymap.set('', ',i', '<cmd>FzfLua Snippets<CR>')
 keymap.set('n', ',g', "<cmd>FzfLua live_grep<CR>")
 keymap.set('n', ',e', "<cmd>FzfLua buffers<CR>")
 keymap.set('n', ',G', "<cmd>FzfLua grep_cword<CR>")
 keymap.set('n', ',h', "<cmd>FzfLua helptags<CR>")
-keymap.set('n', ',d', "<cmd>FzfLua diagnostics<CR>")
+keymap.set('n', ',d', "<cmd>FzfLua diagnostics_workspace<CR>")
 keymap.set('n', ',qfh', "<cmd>FzfLua quickfix_stack<CR>")
+
