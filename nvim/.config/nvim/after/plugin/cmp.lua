c7bebb5cfd153d4b0f29de4bb4a35ce2c04fa1f1
lq201 <qqerqqer@126.com>
2025-03-27 13:26:10 +0800

    Revert "Set codecompanion to auto complete code"
    This reverts commit 79a987e5a8c0850c8068ffb91936815adb65caee.

--- nvim/.config/nvim/after/plugin/cmp.lua
+++ nvim/.config/nvim/after/plugin/cmp.lua
@@ -23,11 +23,15 @@ cmp.setup({
     ['<C-e>'] = cmp.mapping.abort(),
     ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
   }),
-  sources = {
-    per_filetype = {
-      codecompanion = { "codecompanion" },
-    }
-  },
+  sources = cmp.config.sources({
+    { name = 'nvim_lsp' },
+    -- { name = 'vsnip' }, -- For vsnip users.
+    -- { name = 'luasnip' }, -- For luasnip users.
+    { name = 'ultisnips' }, -- For ultisnips users.
+    -- { name = 'snippy' }, -- For snippy users.
+  }, {
+    { name = 'buffer' },
+  })
 })
 
 -- Set configuration for specific filetype.
