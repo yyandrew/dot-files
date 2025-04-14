ca02ce4d7c5080b47ef099acbf0a075f63bf9db7
lq201 <qqerqqer@126.com>
2025-04-14 16:41:02 +0800

    Change to current folder of file

--- nvim/.config/nvim/lua/float-term.lua
+++ nvim/.config/nvim/lua/float-term.lua
@@ -8,6 +8,29 @@ M.term_buf = nil
 -- Terminal window ID
 M.term_win = nil
 
+-- Function to get the directory of the current file
+function M.get_current_file_directory()
+  local current_file = vim.api.nvim_buf_get_name(0)
+  if current_file and current_file ~= "" then
+    return vim.fn.fnamemodify(current_file, ":h")
+  else
+    return vim.fn.getcwd()
+  end
+end
+
+-- Function to create terminal buffer if it doesn't exist
+function M.create_term_buf()
+  -- Create a new terminal buffer with the proper settings
+  local buf = vim.api.nvim_create_buf(false, true)
+
+  -- Set buffer options before terminal creation
+  vim.api.nvim_buf_set_option(buf, 'bufhidden', 'hide')
+  vim.api.nvim_buf_set_option(buf, 'filetype', 'terminal')
+  vim.api.nvim_buf_set_option(buf, 'modifiable', true)
+
+  return buf
+end
+
 -- Function to toggle the floating terminal
 function M.toggle_float_term()
   -- If terminal window exists and is valid, close it
@@ -15,17 +38,6 @@ function M.toggle_float_term()
     vim.api.nvim_win_close(M.term_win, true)
     M.term_win = nil
     return
-  end
-
-  -- Create or find terminal buffer
-  if not M.term_buf or not vim.api.nvim_buf_is_valid(M.term_buf) then
-    -- Create a new terminal buffer with the proper settings
-    M.term_buf = vim.api.nvim_create_buf(false, true)
-
-    -- Set buffer options before terminal creation
-    vim.api.nvim_buf_set_option(M.term_buf, 'bufhidden', 'hide')
-    vim.api.nvim_buf_set_option(M.term_buf, 'filetype', 'terminal')
-    vim.api.nvim_buf_set_option(M.term_buf, 'modifiable', true)
   end
 
   -- Calculate floating window size
@@ -47,26 +59,54 @@ function M.toggle_float_term()
     border = 'rounded'
   }
 
+  -- Create or reuse terminal buffer
+  if not M.term_buf or not vim.api.nvim_buf_is_valid(M.term_buf) then
+    M.term_buf = M.create_term_buf()
+
+    -- Get the directory of the current file
+    local dir = M.get_current_file_directory()
+
+    -- Create terminal with the current file's directory
+    vim.cmd('terminal cd ' .. vim.fn.shellescape(dir) .. ' && ' .. vim.o.shell)
+  else
+    -- Create the floating window first
+    M.term_win = vim.api.nvim_open_win(M.term_buf, true, opts)
+
+    -- Set window-local options
+    vim.api.nvim_win_set_option(M.term_win, 'winhl', 'Normal:Normal')
+    vim.api.nvim_win_set_option(M.term_win, 'winblend', 0)
+
+    -- If terminal exists, send a command to change directory
+    local dir = M.get_current_file_directory()
+    local term_chan = vim.bo[M.term_buf].channel
+
+    if term_chan > 0 then
+      -- Send clear command and change directory
+      vim.api.nvim_chan_send(term_chan, "\clear\n")
+      vim.api.nvim_chan_send(term_chan, "cd " .. vim.fn.shellescape(dir) .. "\n")
+      vim.api.nvim_chan_send(term_chan, "echo 'Changed directory to: " .. dir .. "'\n")
+    end
+
+    vim.cmd('startinsert')
+    return
+  end
+
   -- Create the floating window
   M.term_win = vim.api.nvim_open_win(M.term_buf, true, opts)
 
-  -- Set some window-local options
+  -- Set window-local options
   vim.api.nvim_win_set_option(M.term_win, 'winhl', 'Normal:Normal')
   vim.api.nvim_win_set_option(M.term_win, 'winblend', 0)
 
-  -- Start terminal if not already started
-  if vim.bo[M.term_buf].channel == 0 then
-    vim.fn.termopen(vim.o.shell)
-    vim.cmd('startinsert')
-  else
-    vim.cmd('startinsert')
-  end
+  -- Start insert mode
+  vim.cmd('startinsert')
 end
 
 -- Set up keybinding to toggle floating terminal
-vim.api.nvim_set_keymap('n', ',t', [[<Cmd>lua require('float-term').toggle_float_term()<CR>]], { noremap = true, silent = true })
-vim.api.nvim_set_keymap('t', ',t', [[<C-\><C-n><Cmd>lua require('float-term').toggle_float_term()<CR>]], { noremap = true, silent = true })
-print("float-term loaded")
+vim.api.nvim_set_keymap('n', ',t', [[<Cmd>lua require('float-term').toggle_float_term()<CR>]],
+  { noremap = true, silent = true })
+vim.api.nvim_set_keymap('t', ',t', [[<C-\><C-n><Cmd>lua require('float-term').toggle_float_term()<CR>]],
+  { noremap = true, silent = true })
 
 -- Return module table
 return M
