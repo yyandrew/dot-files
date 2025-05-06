3571f0d2fbf7ce84b212a55990b7ab4f950e27fc
lq201 <qqerqqer@126.com>
2025-05-06 16:15:31 +0800

    feat: add chinese commit generation

--- nvim/.config/nvim/after/plugin/codecompanion.rc.lua
+++ nvim/.config/nvim/after/plugin/codecompanion.rc.lua
@@ -2,6 +2,8 @@ local state, codecompanion = pcall(require, "codecompanion")
 if not state then
   return
 end
+local fmt = string.format
+
 codecompanion.setup({
   adapters = {
     gemini = function()
@@ -44,7 +46,7 @@ codecompanion.setup({
         mapping = "<LocalLeader>ch",
         modes = { "v" },
         index = 11,
-        is_slash_cmd = false,
+        is_slash_cmd = true,
         auto_submit = false,
         short_name = "j2p",
         placement = "replace" -- or "replace"|"add"|"before"|"chat"
@@ -62,6 +64,31 @@ And generate comment above every lines according to the json key.
 ]],
         },
       },
+    },
+    ["Generate Chinese git commit"] = {
+      strategy = "chat",
+      description = "生成中文git提交文案",
+      opts = {
+        index = 12,
+        is_default = true,
+        is_slash_cmd = true,
+        auto_submit = true,
+        short_name = "cn_commit",
+      },
+      prompts = {
+        {
+          role = "user",
+          content = function()
+            return fmt([[
+请帮我生成中文git提交文案,内容是:
+```diff
+%s
+
+```
+前面不要添加`feat`或者`fix`等关键字.]],
+              vim.fn.system("git diff --no-ext-diff --staged"))
+          end
+        } }
     },
   },
 })
