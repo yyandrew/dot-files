8a2e2861fbe34714cb9f5ef3c8ed6f9753cc07b7
lq201 <qqerqqer@126.com>
2025-05-15 16:24:15 +0800

    Add log for copy operation

--- nvim/.config/nvim/lua/utils.lua
+++ nvim/.config/nvim/lua/utils.lua
@@ -1,10 +1,9 @@
--- Add this to your init.lua or create a file like 'float-term.lua' and source it
-
--- Module for floating terminal management
 local M = {}
 function M.get_file_path()
   local lineNum = vim.api.nvim_win_get_cursor(0)[1]
-  vim.fn.setreg('+', vim.fn.expand('%') .. ':' .. lineNum)
+  local fileWithNum = vim.fn.expand('%') .. ':' .. lineNum
+  vim.fn.setreg('+', fileWithNum)
+  vim.notify("copyed: " .. fileWithNum, vim.log.levels.INFO)
 end
 
 vim.keymap.set('n', ',y', [[<Cmd>lua require('utils').get_file_path()<CR>]], { noremap = true, silent = true, desc = "Yank current file path to system clipboard" })    -- " yank current file path to system clipboard
