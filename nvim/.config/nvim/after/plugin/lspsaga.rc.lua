acdb7706afd64b2060834c827d61c0c9c97a0b8b
lq201 <qqerqqer@126.com>
2025-11-05 11:47:05 +0800

    Delete useless lspsaga keys

--- nvim/.config/nvim/after/plugin/lspsaga.rc.lua
+++ nvim/.config/nvim/after/plugin/lspsaga.rc.lua
@@ -11,6 +11,4 @@ lspsaga.setup({
 local keymap = vim.keymap.set
 
 keymap('n', ';gh', '<cmd>Lspsaga lsp_finder<CR>', { desc = "Show the definition, reference and implementation" })
-keymap('n', ';ca', '<cmd>Lspsaga code_action<CR>')
-keymap('n', ';rn', '<cmd>Lspsaga rename<CR>', { desc = "Rename" })
 keymap('n', ';ol', '<cmd>Lspsaga outline<CR>', { desc = "Display outline" })
