a4b647e8e9a1239b6cb5da81421835bed77b3d64
lq201 <qqerqqer@126.com>
2026-02-10 17:19:33 +0800

    Fix opencode display problem in ssh

--- nvim/.config/nvim/lua/plugins.lua
+++ nvim/.config/nvim/lua/plugins.lua
@@ -147,28 +147,15 @@ return {
   },
   {
     "NickvanDyke/opencode.nvim",
-    -- dependencies = {
-      -- -- Recommended for `ask()` and `select()`.
-      -- -- Required for `snacks` provider.
-      -- ---@module 'snacks' <- Loads `snacks.nvim` types for configuration intellisense.
-      -- { "folke/snacks.nvim", opts = { input = {}, picker = {}, terminal = {} } },
-    -- },
+    dependencies = {
+      -- Recommended for `ask()` and `select()`.
+      -- Required for `snacks` provider.
+      ---@module 'snacks' <- Loads `snacks.nvim` types for configuration intellisense.
+      { "folke/snacks.nvim", opts = { input = {}, picker = {}, terminal = {} } },
+    },
     config = function()
       ---@type opencode.Opts
-      vim.g.opencode_opts = {
-        cmd = "opencode --port",
-        terminal = {
-          split = "left",
-          width = math.floor(vim.o.columns * 0.35),
-        },
-        tmux = {
-          options = "-h", -- Open in a horizontal split
-          focus = false, -- Keep focus in Neovim
-          -- Disables allow-passthrough in the tmux split
-          -- preventing OSC escape sequences from leaking into the nvim buffer
-          allow_passthrough = false,
-        },
-      }
+      vim.g.opencode_opts = { }
 
       -- Required for `opts.events.reload`.
       vim.o.autoread = true
